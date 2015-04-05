8004c5362bc70a9fe66dfbdfbb6740810968aa07
Andrew D Benson <drew@adbenson.net>
2015-04-05 19:32:53 -0400

    Added wraparound to the hue function

--- halo/HaloMode.cpp
+++ halo/HaloMode.cpp
@@ -17,7 +17,7 @@ uint32_t HaloMode::rgb(uint8_t r, uint8_t g, uint8_t b) {
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t HaloMode::hue(byte hue) {
-  hue = 255 - hue;
+  hue = 255 - (hue % 255);
   
   if (hue < 85) {
     return rgb(255 - hue * 3, 0, hue * 3);

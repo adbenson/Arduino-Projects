a34dc11a7563037261edd5fee3763ed9f9137692
Andrew D Benson <drew@adbenson.net>
2015-04-05 14:20:34 -0400

    Adding hue method to encode a color from a hue value

--- halo/HaloMode.cpp
+++ halo/HaloMode.cpp
@@ -12,4 +12,21 @@ uint32_t HaloMode::rgb(uint8_t r, uint8_t g, uint8_t b) {
   return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
 }
 
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t HaloMode::hue(byte hue) {
+  hue = 255 - hue;
+  
+  if(hue < 85) {
+    return rgb(85 - hue, 0, hue * 3);
+  } 
+  else if(hue < 170) {
+    hue -= 85;
+    return rgb(0, hue * 1.5, 255 - hue * 3);
+  } 
+  else {
+    hue -= 170;
+    return rgb(hue, 127 - hue * 1.5,   0);
+  }
+}
 
--- halo/HaloMode.h
+++ halo/HaloMode.h
@@ -14,6 +14,7 @@ class HaloMode {
 
                 static uint32_t rgb(uint8_t r, uint8_t g, uint8_t b);
                 
+                static uint32_t hue(byte hue);
                 
 	protected:
                 uint32_t pixels[NUMPIXELS];
